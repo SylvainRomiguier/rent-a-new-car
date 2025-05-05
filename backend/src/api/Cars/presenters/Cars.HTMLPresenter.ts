29324818903a6847e34df793805229b11782b4eb
Sylvain Romiguier <sylvain.romiguier@gmail.com>
2025-05-05 20:12:43 +0200

    feat(layout): add layout container

--- backend/src/api/Cars/presenters/Cars.HTMLPresenter.ts
+++ backend/src/api/Cars/presenters/Cars.HTMLPresenter.ts
@@ -7,19 +7,24 @@ import path from "path";
 export class CarsHtmlPresenter implements IPresenter<CarData[]> {
   presentedValue: string = "";
   private carsTableTemplate: string = "";
+  private html:string;
   constructor() {
     try {
+      
       this.carsTableTemplate = fs.readFileSync(
         path.resolve(__dirname, "../../templates/carsTable.template.hbs"),
         "utf-8"
       );
+      this.html = fs.readFileSync(path.resolve(__dirname, "../../templates/layout.template.hbs"), "utf-8");
     } catch (error) {
       console.error("Error reading template file:", error);
       throw error;
     }
   }
   present(cars: CarData[]): void {
-    this.presentedValue = Handlebars.compile(this.carsTableTemplate)({
+    this.html = this.html.replace("{{ content }}", this.carsTableTemplate);
+    this.presentedValue = Handlebars.compile(this.html)({
+      title: "Cars",
       cars,
     });
   }
