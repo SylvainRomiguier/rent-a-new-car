98f2be5edb7e8520e4d7ede69f3b91c072047146
Sylvain Romiguier <sylvain.romiguier@gmail.com>
2025-05-09 13:12:37 +0200

    fix(build ci): update dockerfile

--- backend/src/api/Car/presenters/Car.HTMLPresenter.ts
+++ backend/src/api/Car/presenters/Car.HTMLPresenter.ts
@@ -1,13 +1,27 @@
 import Handlebars from "handlebars";
-import carsTableTemplate from "../../templates/carsTable.template.hbs";
 import { CarDataWithPendingBookings } from "../../../domain/Car/Car";
 import { IPresenter } from "../../../domain/common/IPresenter";
+import path from "path";
+import fs from "fs";
 
 export class CarHtmlPresenter
   implements IPresenter<CarDataWithPendingBookings>
 {
   presentedValue: string = "";
+  private html: string;
+  constructor() {
+     try {
+         this.html = fs.readFileSync(
+           path.resolve(__dirname, "../../templates/carDetails.template.hbs"),
+           "utf-8"
+         );
+       } catch (error) {
+         console.error("Error reading template file:", error);
+         throw error;
+       }
+  }
+    
   present(car: CarDataWithPendingBookings): void {
-    this.presentedValue = Handlebars.compile(carsTableTemplate)(car);
+    this.presentedValue = Handlebars.compile(this.html)(car);
   }
 }
